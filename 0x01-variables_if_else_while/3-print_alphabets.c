d5ec7501e0636e13e6ceb4cf2ca2372b1e53fd54
George-Itegi <georgeitegi2001@gmail.com>
2023-08-17 14:02:51 +0300

    C programming loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+#include <stdio.h>
+
+/**
+ * main - Printing the alphabet
+ *
+ * Return: Always 0 (Success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar (letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar (letter);
+	putchar ('\n');
+	return (0);
+}
+=======
+#include <stdio.h>
+
+/**
+ * main - Printing the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar (letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	putchar (letter);
+
+	putchar ('\n');
+
+	return (0);
+}
+>>>>>>> fb4d8c1eb764ad2fa3bed04f3855e513f99961ed
